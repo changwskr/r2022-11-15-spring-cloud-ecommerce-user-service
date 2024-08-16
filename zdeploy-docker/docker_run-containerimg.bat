162eee5450f40122103c9fe09df763e5119953c0
changwskr <changwskr@naver.com>
2024-08-16 23:22:01 +0900

    bat 수정
    수정

--- zdeploy-docker/docker_run-containerimg.bat
+++ zdeploy-docker/docker_run-containerimg.bat
@@ -1,4 +1,4 @@
 echo "컨테이너를 특정포터로 open"
-docker run -d -p 8000:8000 --name my-userservice my-userservice
+docker run -d -p 9090:9090 --name my-userservice my-userservice
 
 docker ps | findstr my-userservice
