86b62768dca3c30e0ac4156e19687f65a3b2165a
changwskr <changwskr@naver.com>
2024-08-25 21:15:13 +0900

    08-25

---
+++ zbin/java run.cmd
@@ -0,0 +1,3 @@
+java -Dspring.profiles.active=default xxx.jar
+
+java -jar .\target\config-service-1.0.jar --spring.profiles.active=native
---
+++ zbin/mvn spring-boot run.cmd
@@ -0,0 +1,3 @@
+mvn spring-boot:run -Dspring-boot.run.arguements=--spring.profiles.active=native
+
+java -jar .\target\config-service-1.0.jar --spring.profiles.active=native
